934450f96e766153ba1de03db783c5b60afcbaca
xiatianjin <1760004035@qq.com>
2015-10-10 14:10:20 +0800

    UGUI虚拟周

---
+++ Joystack/Assets/Script/Joystack/GUI_EventTriggrListener.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public delegate void UIDelegate(GameObject go);
+public delegate void UIVectorDelegate(GameObject go,Vector2 delta);
+public class GUI_EventTriggrListener : EventTrigger
+{
+    public UIDelegate OnClickDelegate;
+
+    public UIDelegate OnPointDownDelegate;
+
+    public UIDelegate OnPoinitEnterDelegate;
+
+    public UIDelegate OnUpdateSelectDelegate;
+
+    public UIDelegate OnDragEndDelegate;
+
+    public UIVectorDelegate OnDragDelegate;
+
+    public static GUI_EventTriggrListener GetEventTriggerListener(GameObject go)
+    {
+        if (go == null)
+        {
+            Debug.LogError("Event Trigger listener is NULL");
+            return null;
+        }
+        else
+        {
+            GUI_EventTriggrListener nswellEventTrigger = go.GetComponent<GUI_EventTriggrListener>();
+
+            if (nswellEventTrigger == null)
+            {
+                nswellEventTrigger = go.AddComponent<GUI_EventTriggrListener>();
+            }
+            return nswellEventTrigger;
+        }
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if (OnDragDelegate != null) OnDragDelegate(gameObject, eventData.delta);
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        if (OnDragEndDelegate != null) OnDragEndDelegate(gameObject);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (OnPointDownDelegate != null) OnPointDownDelegate(gameObject);
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        if (OnPoinitEnterDelegate != null) OnPoinitEnterDelegate(gameObject);
+    }
+
+    public override void OnUpdateSelected(BaseEventData eventData)
+    {
+        if (OnUpdateSelectDelegate != null) OnUpdateSelectDelegate(gameObject);
+    }
+}
---
+++ Joystack/Assets/Script/Joystack/GUI_PlayerMoveController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void MoveDelegate();
+public class GUI_PlayerMoveController : MonoBehaviour {
+
+
+
+    public static MoveDelegate moveStart;
+    public static MoveDelegate moveEnd;
+    public static GUI_PlayerMoveController instance;
+
+    [SerializeField]
+    private GUI_JoytackController guiJoystackController;
+    private Transform selfTransform;
+    [SerializeField]
+    private bool turnBase = false;
+    private float angle;
+    [SerializeField]
+    private float moveSpeed;
+
+    private Animation playerAnimation;
+    void Awake()
+    {
+        playerAnimation = GetComponent<Animation>();
+        playerAnimation["wait"].blendMode = AnimationBlendMode.Blend;
+        instance = this;
+        selfTransform = this.transform;
+
+        moveStart = OnMoveStart;
+        moveEnd = OnMoveEnd;
+    }
+
+    void Update()
+    {
+        if (turnBase)
+        {
+            //位置的移动
+            Vector3 move = guiJoystackController.movePosNorm * Time.deltaTime * moveSpeed;
+            selfTransform.localPosition += move;
+            //从JoytackController移动方向 算出自身的角度
+            angle = Mathf.Atan2(guiJoystackController.movePosNorm.x,
+                guiJoystackController.movePosNorm.z) * Mathf.Rad2Deg;
+            selfTransform.localRotation = Quaternion.Euler(Vector3.up * angle);
+
+        }
+    }
+
+
+    private void OnMoveEnd()
+    {
+        turnBase = false;
+        playerAnimation.Play("wait");
+
+    }
+
+    private void OnMoveStart()
+    {
+        turnBase = true;
+        playerAnimation.Play("run");
+     
+    }
+}
